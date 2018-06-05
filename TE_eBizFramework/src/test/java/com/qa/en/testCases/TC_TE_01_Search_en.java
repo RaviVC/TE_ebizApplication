9e2867dbe9e2b11478560fb64d303fad6b72bfd4
RaviChandrika <ravi.venkateshachandrika@te.com>
2018-06-05 18:58:04 +0530

    Second Commit

---
+++ TE_eBizFramework/src/test/java/com/qa/en/testCases/TC_TE_01_Search_en.java
@@ -0,0 +1,35 @@
+package com.qa.en.testCases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TC_TE_01_Search_en {
+	
+	WebDriver driver;
+	
+	@BeforeTest
+	public void setUp()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void Search_2mm()
+	{	
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.get("http://www-stage.te.com/usa-en/home.html");
+		System.out.println("URL Launched successfully");
+	}
+	
+	@AfterTest
+	public void TearDown()
+	{
+		driver.quit();
+	}
+}
+
